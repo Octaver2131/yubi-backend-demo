087471794c54a8acd000b203aafcf7866fae9922
Octaver <Octaver2131@outlook.com>
2025-06-17 23:18:29 +0800

    1:n 模型

---
+++ src/main/java/com/yupi/springbootinit/mq/MultiConsumer.java
@@ -0,0 +1,51 @@
+package com.yupi.springbootinit.mq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class MultiConsumer {
+
+    private static final String TASK_QUEUE_NAME = "multi_queue";
+
+    public static void main(String[] argv) throws Exception {
+        // 建立连接
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        final Connection connection = factory.newConnection();
+        for (int i = 0; i < 2; i++) {
+            final Channel channel = connection.createChannel();
+
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+            System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+            channel.basicQos(1);
+
+            // 定义了如何处理消息
+            int finalI = i;
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), "UTF-8");
+
+                try {
+                    // 处理工作
+                    System.out.println(" [x] Received '" + "编号:" + finalI + ":" + message + "'");
+                    // try 块和 finally 块中都调用了 basicAck ，这会导致对同一条消息进行多次确认，从而引发异常。
+                    // channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                    // 停 5 秒，模拟机器处理能力有限
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    // 拒绝消息，requeue 参数为 true 表示将消息重新入队列
+                    channel.basicNack(delivery.getEnvelope().getDeliveryTag(), false, false);
+                } finally {
+                    System.out.println(" [x] Done");
+                    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                }
+            };
+            // 开启消费监听
+            channel.basicConsume(TASK_QUEUE_NAME, false, deliverCallback, consumerTag -> {
+            });
+        }
+    }
+}
